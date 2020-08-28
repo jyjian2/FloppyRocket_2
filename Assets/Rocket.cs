82e966af6d507428462032a356c4b8fde35e23d9
Jia-Yi Jian <jyjian@illinois.edu>
2020-08-28 14:05:45 -0500

    Added audio sound when user thrusting the rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,11 +7,15 @@ public class Rocket : MonoBehaviour
     //reference rigidbody in this script
     //Crearte a variable of type of Rigidbody
     Rigidbody rigidBody;
+
+    //Created a variable of type of audiosource
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         //Ask Only to act on the component of ridgidbody(*genetics)
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,21 +26,31 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        //if user input is pressing space, then thrust the rocket
+        // if user input is pressing space, then thrust the rocket
         if (Input.GetKey(KeyCode.Space))
         {
-            //type . can explore what we can do to the variable rigidBody
-            //Use relative force means it thrust based on the object itself
-            //vecter3 means the position x, y, and z 
+            // type . can explore what we can do to the variable rigidBody
+            // Use relative force means it thrust based on the object itself
+            // vecter3 means the position x, y, and z 
             rigidBody.AddRelativeForce(Vector3.up);
+            // Control not playing the audio eahc time when user press space. 
+            // It should only start when the user press apce and when there's no sound is playing
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Stop();
+            }    
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left"); 
+            transform.Rotate(Vector3.forward);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
