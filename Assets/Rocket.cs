91cbaba4cd0c0990a76663bc8aef0653c1d4a336
Jia-Yi Jian <jyjian@illinois.edu>
2020-08-30 10:51:49 -0500

    Put three different sound according to game state

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,6 +7,9 @@ public class Rocket : MonoBehaviour
     // Make thrust that we can modified to become an inspector in the of the object
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
     
     //reference rigidbody in this script
     //Crearte a variable of type of Rigidbody
@@ -32,14 +35,14 @@ public class Rocket : MonoBehaviour
         //Add this condition to allow user to only play somthing when the status is Alive
         if (state == State.Alive)
         {
-            Thrust();
-            Rotate();
+            RespondToThrustInput();
+            RespondToRotateInput();
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (state != State.Alive){return;} // ignore collision when dead
+        if (state != State.Alive){ return; } // ignore collision when dead
         // Switch based on the collision comes in, collision.ganeObject means you look at the game object you're collliding with
         // and reading its tag and switch based on the string.
         switch (collision.gameObject.tag)
@@ -49,18 +52,32 @@ public class Rocket : MonoBehaviour
                 break;
 
             case "Finish":
-                state = State.Transcending;
-                Invoke("LoadNextLevel", 1f); 
-                // called routine. Points down to the method "LoadNextLevel" after one second
+                StartSuccessSequence();
                 break;
 
             default:
-                print("Hit something deadly");
-                state = State.Dying;
-                Invoke("LoadFirstLevel", 1f); // parameterise time
+                StartDeathSequence();
                 break;
         }
     }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", 1f);
+        // called routine. Points down to the method "LoadNextLevel" after one second
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f); // parameterise time
+    }
+
 
     private void LoadNextLevel()
     {
@@ -73,34 +90,41 @@ public class Rocket : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         // if user input is pressing space, then thrust the rocket
         if (Input.GetKey(KeyCode.Space))
         {
-            // type . can explore what we can do to the variable rigidBody
-            // Use relative force means it thrust based on the object itself
-            // vecter3 means the position x, y, and z 
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            // Control not playing the audio eahc time when user press space. 
-            // It should only start when the user press apce and when there's no sound is playing
+            ApplyThrust();
         }
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
         else
         {
             audioSource.Stop();
         }    
-    }    
-    private void Rotate()
+    }
+
+    private void ApplyThrust()
+    {
+        // type . can explore what we can do to the variable rigidBody
+        // Use relative force means it thrust based on the object itself
+        // vecter3 means the position x, y, and z 
+        // Control not playing the audio eahc time when user press space. 
+        // It should only start when the user press apce and when there's no sound is playing
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) // so it doesn't layer
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         //control whether physics will change the rotation of the object
         //Take manual control of rotation
-        rigidBody.freezeRotation = true;
+        rigidBody.freezeRotation = true; //take mainual control of rotation
 
-        
+        // if user input is pressing space, then thrust the rocket
+
         float rotateThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
@@ -113,7 +137,7 @@ public class Rocket : MonoBehaviour
         }
         
         rigidBody.freezeRotation = false; // resume physics control of rotation
-    }
+    }    
         
 }
 // Time.deltaTime tells us the last frame time, so it's good predictor of the current frame time
